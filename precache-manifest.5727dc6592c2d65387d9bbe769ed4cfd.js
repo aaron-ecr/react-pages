6cbef8f8f9b307929fe2529783f5d9084f1e8be2
aaron-ecr <aaron.espi06@gmail.com>
2022-01-28 00:22:20 -0600

    Updates

---
+++ precache-manifest.5727dc6592c2d65387d9bbe769ed4cfd.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d2b0d68a6dd797e7f9575ecfa18284",
+    "url": "/react-pages/index.html"
+  },
+  {
+    "revision": "cb5a0e854834647d6b1a",
+    "url": "/react-pages/static/css/2.dad981ca.chunk.css"
+  },
+  {
+    "revision": "0bc0361cac3ce2a50ec9",
+    "url": "/react-pages/static/css/main.86a1874c.chunk.css"
+  },
+  {
+    "revision": "cb5a0e854834647d6b1a",
+    "url": "/react-pages/static/js/2.90480be3.chunk.js"
+  },
+  {
+    "revision": "0bc0361cac3ce2a50ec9",
+    "url": "/react-pages/static/js/main.3eca81e1.chunk.js"
+  },
+  {
+    "revision": "1e067ef579591dacb6c4",
+    "url": "/react-pages/static/js/runtime~main.2afdb07a.js"
+  },
+  {
+    "revision": "a79151a596a81b8bb53354676ed9002e",
+    "url": "/react-pages/static/media/404.a79151a5.svg"
+  },
+  {
+    "revision": "d58eac18ff5955fc086045d5f229ec96",
+    "url": "/react-pages/static/media/airline3.d58eac18.jpg"
+  },
+  {
+    "revision": "8bad1bca6f117edbb3153d5e861aac8c",
+    "url": "/react-pages/static/media/astronaut.8bad1bca.svg"
+  },
+  {
+    "revision": "ac33bef71984f7aa8633ef18800d7ea6",
+    "url": "/react-pages/static/media/carrito2.ac33bef7.png"
+  },
+  {
+    "revision": "81ccb8ef84e8be0dc7ca1bf90b2d29f5",
+    "url": "/react-pages/static/media/earth.81ccb8ef.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-pages/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "6a07188b3cc1689698b796434395c3dc",
+    "url": "/react-pages/static/media/modalConfirm3.6a07188b.png"
+  },
+  {
+    "revision": "fcf33db651ced3d705c5011f6bf97382",
+    "url": "/react-pages/static/media/moon.fcf33db6.svg"
+  },
+  {
+    "revision": "137f47fb1c33ffe4755720950d1ebf4d",
+    "url": "/react-pages/static/media/multiflight.137f47fb.png"
+  },
+  {
+    "revision": "215e612c7a902f98243506aca7c9b642",
+    "url": "/react-pages/static/media/nonstop.215e612c.png"
+  },
+  {
+    "revision": "4d329bba1aeb9e3567f85e6c003452b8",
+    "url": "/react-pages/static/media/rocket.4d329bba.svg"
+  },
+  {
+    "revision": "0088ecaa708d52f3198524180fbcc96e",
+    "url": "/react-pages/static/media/shopping-cart.0088ecaa.png"
+  },
+  {
+    "revision": "433b51aa2b11560e81444a85590a770b",
+    "url": "/react-pages/static/media/world_airlines.433b51aa.png"
+  }
+]);
